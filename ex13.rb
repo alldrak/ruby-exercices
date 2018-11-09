ee894ff9206090119cb6c83e71565816b096841c
Alexandru Voinea <gordonsrap@gmail.com>
2018-11-09 01:58:12 +0100

    change method of atgv and stds

--- ex13.rb
+++ ex13.rb
@@ -1,3 +1,4 @@
+=begin
 puts "Input 1"
 first = $stdin.gets.chomp
 puts "Input 2"
@@ -5,6 +6,9 @@ second = $stdin.gets.chomp
 puts "Input 3"
 third = $stdin.gets.chomp
 fourth = ARGV
+=end
+
+first, second, third, fourth = ARGV
 
 puts "Your first variable is: #{first}."
 puts "Your sencond variable is: #{second}."
