bc98ed107561b978e28b635ab4776e26045788ce
Alexandru Voinea <gordonsrap@gmail.com>
2018-11-09 09:18:25 +0100

    add and solved ex14 and iterations

--- ex14.rb
+++ ex14.rb
@@ -1,5 +1,6 @@
-user_name = ARGV.first #gets first argument 
-prompt = '> '
+# user_name = ARGV.first #gets first argument 
+user_name, last_name = ARGV #gets second argument
+prompt = '----- '
 
 puts "Hi #{user_name}."
 puts "I'd like to ask you a few questions."
@@ -18,5 +19,6 @@ computer = $stdin.gets.chomp
 puts """
 Alright, so you said #{likes} about liking me.
 You live in #{lives}. Not sure whare that is. 
-And you have a #{computer} computer. Nice.
+You have a #{computer} computer. Nice.
+And your last name is #{last_name}. Weird right? 
 """
