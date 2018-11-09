14340f8695fea31f738149c2f3b3aea613c27e02
Alexandru Voinea <gordonsrap@gmail.com>
2018-11-09 01:38:39 +0100

    add ex13 and modify input method

--- ex13.rb
+++ ex13.rb
@@ -1,4 +1,9 @@
-first, second, third = ARGV
+puts "Input 1"
+first = $stdin.gets.chomp
+puts "Input 2"
+second = $stdin.gets.chomp
+puts "Input 3"
+third = $stdin.gets.chomp
 
 puts "Your first variable is: #{first}."
 puts "Your sencond variable is: #{second}."
